5051269fb8f2795e12cae53bff73fc5c78168b22
xtristrix <xtristrix@cyberiade.de>
2017-12-19 17:26:18 +0100

    Initial commit

---
+++ gen.py
@@ -0,0 +1,6 @@
+import tkinter
+def generate():
+    foodFrame = tkinter.Tk()
+    lblFood = tkinter.Label(foodFrame, text="mach dir doch än schinkenkäsesandwich auf wiezenbrot")
+    lblFood.pack()
+    foodFrame.mainloop()
---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter
+import gen
+
+generate = gen.generate
+
+main = tkinter.Tk()
+
+btnGenerator = tkinter.Button(main, text="generate", command=generate)
+btnGenerator.pack()
+
+
+main.mainloop()
